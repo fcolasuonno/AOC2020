3fbb1482797e7cabadc380c9facb62df7cd9b7f6
fcolasuonno <f.colasuonno@gmail.com>
2020-11-30 19:31:54 +0000

    Initial template

---
+++ src/main/kotlin/day0N/Code.kt
@@ -0,0 +1,30 @@
+package day0N
+
+import isDebug
+import java.io.File
+
+fun main() {
+    val name = if (isDebug()) "test.txt" else "input.txt"
+    System.err.println(name)
+    val dir = ::main::class.java.`package`.name
+    val input = File("src/main/kotlin/$dir/$name").readLines()
+    val parsed = parse(input)
+    part1(parsed)
+//    part2(parsed)
+}
+
+fun parse(input: List<String>) = input.map {
+    it.toInt()
+}.requireNoNulls()
+
+fun part1(input: List<Int>) {
+    val res = input.sumBy { it / 3 - 2 }
+    println("Part 1 = $res")
+}
+
+fun part2(input: List<Int>) {
+    val res = input.sumBy { mass ->
+        generateSequence(mass) { fuel -> (fuel / 3 - 2).takeIf { it > 0 } }.drop(1).sum()
+    }
+    println("Part 2 = $res")
+}
