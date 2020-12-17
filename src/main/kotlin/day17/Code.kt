99b951385a7057084b0397dad17af3b70f1b3962
fcolasuonno <f.colasuonno@gmail.com>
2020-12-17 06:34:31 +0000

    Day 17 - more readable

--- src/main/kotlin/day17/Code.kt
+++ src/main/kotlin/day17/Code.kt
@@ -4,35 +4,13 @@ import isDebug
 import java.io.File
 
 private val Triple<Int, Int, Int>.neighbours: Set<Triple<Int, Int, Int>>
-    get() = setOf(
-        Triple(first - 1, second - 1, third - 1),
-        Triple(first - 1, second - 1, third),
-        Triple(first - 1, second - 1, third + 1),
-        Triple(first - 1, second, third - 1),
-        Triple(first - 1, second, third),
-        Triple(first - 1, second, third + 1),
-        Triple(first - 1, second + 1, third - 1),
-        Triple(first - 1, second + 1, third),
-        Triple(first - 1, second + 1, third + 1),
-        Triple(first, second - 1, third - 1),
-        Triple(first, second - 1, third),
-        Triple(first, second - 1, third + 1),
-        Triple(first, second, third - 1),
-//        Triple(first,second,third),
-        Triple(first, second, third + 1),
-        Triple(first, second + 1, third - 1),
-        Triple(first, second + 1, third),
-        Triple(first, second + 1, third + 1),
-        Triple(first + 1, second - 1, third - 1),
-        Triple(first + 1, second - 1, third),
-        Triple(first + 1, second - 1, third + 1),
-        Triple(first + 1, second, third - 1),
-        Triple(first + 1, second, third),
-        Triple(first + 1, second, third + 1),
-        Triple(first + 1, second + 1, third - 1),
-        Triple(first + 1, second + 1, third),
-        Triple(first + 1, second + 1, third + 1),
-    )
+    get() = (-1..1).flatMap { d1 ->
+        (-1..1).flatMap { d2 ->
+            (-1..1).map { d3 ->
+                Triple(first + d1, second + d2, third + d3)
+            }
+        }
+    }.filter { it != this }.toSet()
 
 fun main() {
     val name = if (isDebug()) "test.txt" else "input.txt"
@@ -44,29 +22,32 @@ fun main() {
     part2(parsed)
 }
 
-
 fun parse(input: List<String>) = input.flatMapIndexed { y, s ->
     s.mapIndexedNotNull { x, c -> c.takeIf { it == '#' }?.let { Triple(x, y, 0) } }
 }.toSet()
 
 fun part1(input: Set<Triple<Int, Int, Int>>) {
     var start = input
+    fun Triple<Int, Int, Int>.neighboursCount() = neighbours.count { it in start }
     repeat(6) {
-        val active1 = start.filter { each -> each.neighbours.count { it in start }.let { it == 2 || it == 3 } }.toSet()
-        val active2 = start.flatMap { it.neighbours }.toSet()
-            .filter { each -> each !in start && each.neighbours.count { it in start } == 3 }.toSet()
-        start = active1 + active2
+        start = mutableSetOf<Triple<Int, Int, Int>>().apply {
+            start.filterTo(this) { each -> each.neighboursCount().let { it == 2 || it == 3 } }
+            start.flatMapTo(mutableSetOf()) { it.neighbours }
+                .filterTo(this) { each -> each !in start && each.neighboursCount() == 3 }
+        }
     }
     println("Part 1 = ${start.size}")
 }
 
 fun part2(input: Set<Triple<Int, Int, Int>>) {
-    var start = input.map { Quadruple(it.first, it.second, it.third, 0) }.toSet()
+    var start = input.mapTo(mutableSetOf()) { Quadruple(it.first, it.second, it.third, 0) }
+    fun Quadruple.neighboursCount() = neighbours.count { it in start }
     repeat(6) {
-        val active1 = start.filter { each -> each.neighbours.count { it in start }.let { it == 2 || it == 3 } }.toSet()
-        val active2 = start.flatMap { it.neighbours }.toSet()
-            .filter { each -> each !in start && each.neighbours.count { it in start } == 3 }.toSet()
-        start = active1 + active2
+        start = mutableSetOf<Quadruple>().apply {
+            start.filterTo(this) { each -> each.neighboursCount().let { it == 2 || it == 3 } }
+            start.flatMapTo(mutableSetOf()) { it.neighbours }
+                .filterTo(this) { each -> each !in start && each.neighboursCount() == 3 }
+        }
     }
     println("Part 2 = ${start.size}")
 }
@@ -78,89 +59,13 @@ data class Quadruple(
     val fourth: Int
 ) {
     val neighbours: Set<Quadruple>
-        get() = setOf(
-            Quadruple(first - 1, second - 1, third - 1, fourth - 1),
-            Quadruple(first - 1, second - 1, third, fourth - 1),
-            Quadruple(first - 1, second - 1, third + 1, fourth - 1),
-            Quadruple(first - 1, second, third - 1, fourth - 1),
-            Quadruple(first - 1, second, third, fourth - 1),
-            Quadruple(first - 1, second, third + 1, fourth - 1),
-            Quadruple(first - 1, second + 1, third - 1, fourth - 1),
-            Quadruple(first - 1, second + 1, third, fourth - 1),
-            Quadruple(first - 1, second + 1, third + 1, fourth - 1),
-            Quadruple(first, second - 1, third - 1, fourth - 1),
-            Quadruple(first, second - 1, third, fourth - 1),
-            Quadruple(first, second - 1, third + 1, fourth - 1),
-            Quadruple(first, second, third - 1, fourth - 1),
-            Quadruple(first, second, third, fourth - 1),
-            Quadruple(first, second, third + 1, fourth - 1),
-            Quadruple(first, second + 1, third - 1, fourth - 1),
-            Quadruple(first, second + 1, third, fourth - 1),
-            Quadruple(first, second + 1, third + 1, fourth - 1),
-            Quadruple(first + 1, second - 1, third - 1, fourth - 1),
-            Quadruple(first + 1, second - 1, third, fourth - 1),
-            Quadruple(first + 1, second - 1, third + 1, fourth - 1),
-            Quadruple(first + 1, second, third - 1, fourth - 1),
-            Quadruple(first + 1, second, third, fourth - 1),
-            Quadruple(first + 1, second, third + 1, fourth - 1),
-            Quadruple(first + 1, second + 1, third - 1, fourth - 1),
-            Quadruple(first + 1, second + 1, third, fourth - 1),
-            Quadruple(first + 1, second + 1, third + 1, fourth - 1),
-
-            Quadruple(first - 1, second - 1, third - 1, fourth),
-            Quadruple(first - 1, second - 1, third, fourth),
-            Quadruple(first - 1, second - 1, third + 1, fourth),
-            Quadruple(first - 1, second, third - 1, fourth),
-            Quadruple(first - 1, second, third, fourth),
-            Quadruple(first - 1, second, third + 1, fourth),
-            Quadruple(first - 1, second + 1, third - 1, fourth),
-            Quadruple(first - 1, second + 1, third, fourth),
-            Quadruple(first - 1, second + 1, third + 1, fourth),
-            Quadruple(first, second - 1, third - 1, fourth),
-            Quadruple(first, second - 1, third, fourth),
-            Quadruple(first, second - 1, third + 1, fourth),
-            Quadruple(first, second, third - 1, fourth),
-//            Quadruple(first, second, third, fourth),
-            Quadruple(first, second, third + 1, fourth),
-            Quadruple(first, second + 1, third - 1, fourth),
-            Quadruple(first, second + 1, third, fourth),
-            Quadruple(first, second + 1, third + 1, fourth),
-            Quadruple(first + 1, second - 1, third - 1, fourth),
-            Quadruple(first + 1, second - 1, third, fourth),
-            Quadruple(first + 1, second - 1, third + 1, fourth),
-            Quadruple(first + 1, second, third - 1, fourth),
-            Quadruple(first + 1, second, third, fourth),
-            Quadruple(first + 1, second, third + 1, fourth),
-            Quadruple(first + 1, second + 1, third - 1, fourth),
-            Quadruple(first + 1, second + 1, third, fourth),
-            Quadruple(first + 1, second + 1, third + 1, fourth),
-
-            Quadruple(first - 1, second - 1, third - 1, fourth + 1),
-            Quadruple(first - 1, second - 1, third, fourth + 1),
-            Quadruple(first - 1, second - 1, third + 1, fourth + 1),
-            Quadruple(first - 1, second, third - 1, fourth + 1),
-            Quadruple(first - 1, second, third, fourth + 1),
-            Quadruple(first - 1, second, third + 1, fourth + 1),
-            Quadruple(first - 1, second + 1, third - 1, fourth + 1),
-            Quadruple(first - 1, second + 1, third, fourth + 1),
-            Quadruple(first - 1, second + 1, third + 1, fourth + 1),
-            Quadruple(first, second - 1, third - 1, fourth + 1),
-            Quadruple(first, second - 1, third, fourth + 1),
-            Quadruple(first, second - 1, third + 1, fourth + 1),
-            Quadruple(first, second, third - 1, fourth + 1),
-            Quadruple(first, second, third, fourth + 1),
-            Quadruple(first, second, third + 1, fourth + 1),
-            Quadruple(first, second + 1, third - 1, fourth + 1),
-            Quadruple(first, second + 1, third, fourth + 1),
-            Quadruple(first, second + 1, third + 1, fourth + 1),
-            Quadruple(first + 1, second - 1, third - 1, fourth + 1),
-            Quadruple(first + 1, second - 1, third, fourth + 1),
-            Quadruple(first + 1, second - 1, third + 1, fourth + 1),
-            Quadruple(first + 1, second, third - 1, fourth + 1),
-            Quadruple(first + 1, second, third, fourth + 1),
-            Quadruple(first + 1, second, third + 1, fourth + 1),
-            Quadruple(first + 1, second + 1, third - 1, fourth + 1),
-            Quadruple(first + 1, second + 1, third, fourth + 1),
-            Quadruple(first + 1, second + 1, third + 1, fourth + 1),
-        )
+        get() = (-1..1).flatMap { d1 ->
+            (-1..1).flatMap { d2 ->
+                (-1..1).flatMap { d3 ->
+                    (-1..1).map { d4 ->
+                        Quadruple(first + d1, second + d2, third + d3, fourth + d4)
+                    }
+                }
+            }
+        }.filter { it != this }.toSet()
 }
