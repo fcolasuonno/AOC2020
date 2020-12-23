cb15cb10c1c750681dfa8ef3797b54fb2b95c149
fcolasuonno <f.colasuonno@gmail.com>
2020-12-23 13:15:20 +0000

    Day 23 - optimized

--- src/main/kotlin/day23/Code.kt
+++ src/main/kotlin/day23/Code.kt
@@ -16,49 +16,38 @@ fun main() {
 fun parse(input: List<String>) = input.single().map { it - '0' }
 
 fun part1(input: List<Int>) {
-    val cups = (input + input.first()).zipWithNext().toMap().toMutableMap()
-    val res = generateSequence(input.first()) { index ->
-        val p1 = cups[index]!!
-        val p2 = cups[p1]!!
-        val p3 = cups[p2]!!
-        val next = cups[p3]!!
-        var destination = index
-        do {
-            destination--
-            if (destination == 0) destination = 9
-        } while (destination == p1 || destination == p2 || destination == p3)
-        cups[index] = next
-        cups[p3] = cups[destination]!!
-        cups[destination] = p1
-        next
-    }.take(100 + 1).last().let {
-        generateSequence(cups[1]) { cups[it] }.take(8).joinToString("")
-    }
+    val res = (input + input.first()).play().take(8).joinToString("")
     println("Part 1 = $res")
 }
 
 fun part2(input: List<Int>) {
-    val max = 1000000
-    val cups = (input + (10..max) + input.first()).zipWithNext().toMap().toMutableMap()
-    val res = generateSequence(input.first()) { index ->
-        val p1 = cups[index]!!
-        val p2 = cups[p1]!!
-        val p3 = cups[p2]!!
-        val next = cups[p3]!!
-        var destination = index
-        do {
-            destination--
-            if (destination == 0) destination = max
-        } while (destination == p1 || destination == p2 || destination == p3)
-        cups[index] = next
-        cups[p3] = cups[destination]!!
-        cups[destination] = p1
-        next
-    }.take(10000000 + 1).last().let {
-        cups.let {
-            val first = it.getValue(1)
-            first.toLong() * it.getValue(first)
-        }
-    }
+    val res = (input + 10).play(1000000, 10000000).take(2).fold(1L) { acc, i -> acc * i }
     println("Part 2 = $res")
 }
+
+private fun List<Int>.play(num: Int = size - 1, rounds: Int = 100) = IntArray(num) { it + 1 }
+    .also { list ->
+        map { it - 1 }.zipWithNext().forEach { list[it.first] = it.second }
+        var index = first() - 1
+        if (num > size) {
+            list[list.size - 1] = index
+        }
+        repeat(rounds) {
+            val p1 = list[index]
+            val p2 = list[p1]
+            val p3 = list[p2]
+            val next = list[p3]
+            var destination = index
+            do {
+                destination--
+                if (destination == -1) destination = (list.size - 1)
+            } while (destination == p1 || destination == p2 || destination == p3)
+            list[index] = next
+            list[p3] = list[destination]
+            list[destination] = p1
+            index = next
+        }
+    }.let { list ->
+        generateSequence(list[0]) { list[it] }.map { it + 1 }
+    }
+
